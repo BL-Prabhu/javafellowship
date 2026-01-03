6880bed31a1e2523a1611de90ad006f145b7c7d0
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-01-03 14:39:15 +0530

    FEAT : added UC-2 OOPS-Banner-App

--- oopsbannerapp/Oopsbannerapp.java
+++ oopsbannerapp/Oopsbannerapp.java
@@ -20,10 +20,68 @@ public class Oopsbannerapp
         System.out.println("\"OOPS\" Object Oriented Programming System\n");
     }
 
+    public static void Uc2()
+    {
+
+        String[] O = {
+                "  ******  ",
+                " **    ** ",
+                "**      **",
+                "**      **",
+                "**      **",
+                "**      **",
+                "**      **",
+                " **    ** ",
+                "  ******  "
+        };
+
+        String[] P =
+                {
+                        "*********  ",
+                        "**     ** ",
+                        "**     ** ",
+                        "**     **",
+                        "********  ",
+                        "**         ",
+                        "**         ",
+                        "**         ",
+                        "**         "
+                };
+
+        String[] S =
+                {
+                        "   *****  ",
+                        " **       ",
+                        "**        ",
+                        "  **      ",
+                        "    ***   ",
+                        "       ** ",
+                        "        **",
+                        "       **",
+                        "  *****   "
+
+                };
+
+        for (int i = 0; i < O.length; i++)
+        {
+            System.out.println(
+                    O[i] + "   " +
+                            O[i] + "   " +
+                            P[i] + "   " +
+                            S[i]
+            );
+        }
+    }
+
+
     public static void main(String[] args)
     {
         System.out.println("USE-CASE-1");
         System.out.println("----------------------------------------------------------------------------------------");
         Uc1();
+        System.out.println("USE-CASE-2");
+        System.out.println("----------------------------------------------------------------------------------------");
+        Uc2();
+
     }
 }
