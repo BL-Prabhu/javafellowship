8783d511f32fb91a01d4f3ddf4d684d058e2d932
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-01-23 17:55:35 +0530

    FEAT : addedd DAY-11,12 (Bank_account, Deck_of_cards, Stock_account_management)

---
+++ DAY-11,12/stockaccountmanagement/StockPortfolio.java
@@ -0,0 +1,33 @@
+package stockaccountmanagement;
+
+import java.util.ArrayList;
+
+public class StockPortfolio
+{
+    private ArrayList<Stock> stocks = new ArrayList<>();
+
+    public void addStock(Stock stock) {
+        stocks.add(stock);
+    }
+
+    public double getTotalPortfolioValue()
+    {
+        double total = 0;
+        for (Stock s : stocks)
+        {
+            total += s.getStockValue();
+        }
+        return total;
+    }
+
+    public void printReport()
+    {
+        System.out.println(" STOCK REPORT ");
+        for (Stock s : stocks)
+        {
+            s.printStockInfo();
+        }
+        System.out.println("Total Portfolio Value: " + getTotalPortfolioValue());
+
+    }
+}
